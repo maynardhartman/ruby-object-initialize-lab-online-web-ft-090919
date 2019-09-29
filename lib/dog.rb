8851563901ff3c70726eb69732f436e9144833fe
Maynard Hartman <mjhartman@brighthouse.com>
2019-09-29 10:59:30 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,9 @@
+class Dog 
+  def initialize(the_dogs_name, the_dogs_breed)
+    @name = the_dogs_name if the_dogs_name ![]
+    @breed = the_dogs_breed if the_dogs_breed ![]
+    if @breed = [] then @breed = "Mutt"
+  end
+end
+end
+
