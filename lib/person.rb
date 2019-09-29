d7976298511eb3916bd82c5e2dc6b0cc2149cd47
Maynard Hartman <mjhartman@brighthouse.com>
2019-09-29 10:59:13 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++
@@ -1,9 +0,0 @@
-class Person 
-  def initialize(the_dogs_name, the_dogs_breed)
-    @name = the_dogs_name if the_dogs_name ![]
-    @breed = the_dogs_breed if the_dogs_breed ![]
-    if @breed = [] then @breed = "Mutt"
-  end
-end
-end
-
