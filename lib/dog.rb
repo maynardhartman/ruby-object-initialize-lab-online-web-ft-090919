af6550cbec32fc0d3be653c952d35f5cda9e69b8
Maynard Hartman <mjhartman@brighthouse.com>
2019-09-29 11:17:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,10 @@
 class Dog 
   def initialize(name, breed) 
     if breed == "" then @breed = "Mutt"
+  else 
+    if breed != "" then @breed = breed
   end
     @name = name if name != ""
-    @breed = breed if breed != ""
   end
 end
 
