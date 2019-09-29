eb76a98e3b07bb385743d74f94478072eeae0210
Maynard Hartman <mjhartman@brighthouse.com>
2019-09-29 11:01:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,5 @@
 class Dog 
-  def initialize(the_dogs_name, the_dogs_breed)
+  def initialize(the_dogs_name, the_dogs_breed) do
     @name = the_dogs_name if the_dogs_name ![]
     @breed = the_dogs_breed if the_dogs_breed ![]
     if @breed = [] then @breed = "Mutt"
